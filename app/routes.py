a6ede5210f5fcc0a0235b0e8b89c0568a32160c8
Lucas B. S. <lucasbonato4546@gmail.com>
2025-09-13 11:32:15 -0300

    Atualiza routes para pagainas de erro

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,7 @@
 # app/routes.py
 
 from urllib.parse import urlsplit
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, abort
 import sqlalchemy as sa
 from app import app, db
 from datetime import datetime, timezone
@@ -11,17 +11,33 @@ def index():
     return render_template('chat.html')
 
 @app.route('/chat')
-def landing():
+def chat():
     return render_template('chat.html')
 
 @app.route('/chamados')
-def landing():
+def chamados():
     return render_template('chamados.html')
 
 @app.route('/dashboard')
-def landing():
+def dashboard():
     return render_template('dashboard.html')
 
 @app.route('/custom')
-def landing():
+def custom():
     return render_template('custom.html')
+
+@app.route("/erro_404")
+def erro_404():
+    abort(404)
+
+@app.route("/erro_500")
+def erro_500():
+    abort(500)
+
+@app.route("/erro_403")
+def erro_403():
+    abort(403)
+
+@app.route("/erro_generico")
+def erro_418():
+    abort(418)
